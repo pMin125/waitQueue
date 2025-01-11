6fa0847a202308e7832d69dd242024e3bf90793d
min <min1225y@gmail.com>
2025-01-11 15:39:37 +0900

    진입 api 중간커밋

--- flow/src/main/java/com/project2/flow/controller/UserQueueController.java
+++ flow/src/main/java/com/project2/flow/controller/UserQueueController.java
@@ -1,12 +1,12 @@
 package com.project2.flow.controller;
 
+
+import com.project2.flow.dto.AllowUserResponse;
+import com.project2.flow.dto.AllowedUserResponse;
 import com.project2.flow.dto.RegisterUserResponse;
 import com.project2.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -16,9 +16,23 @@ public class UserQueueController {
     private final UserQueueService userQueueService;
 
     @PostMapping("")
-        public Mono<RegisterUserResponse> registerUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
-                                                       @RequestParam(name = "user_id") Long userId){
-            return userQueueService.registerWaitQueue(queue, userId)
-                    .map(RegisterUserResponse::new);
-        }
+    public Mono<RegisterUserResponse> registerUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                   @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.registerWaitQueue(queue, userId)
+                .map(RegisterUserResponse::new);
+    }
+
+    @PostMapping("/allow")
+    public Mono<AllowUserResponse> allowUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                             @RequestParam(name = "count") Long count) {
+        return userQueueService.allowUser(queue, count)
+                .map(allowed -> new AllowUserResponse(count, allowed));
+    }
+
+    @GetMapping("/allowed")
+    public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                   @RequestParam(name = "user_id") Long userId){
+        return userQueueService.isAllowed(queue,userId)
+                .map(AllowedUserResponse::new);
+    }
 }
---
+++ flow/src/main/java/com/project2/flow/dto/AllowUserResponse.java
@@ -0,0 +1,4 @@
+package com.project2.flow.dto;
+
+public record AllowUserResponse(Long requestCount, Long allowedCount) {
+}
