0e12f4eb8eb62140a9b45e1591db22d3019e2184
min <min1225y@gmail.com>
2025-01-14 21:35:08 +0900

    대기후에 메인페이지

---
+++ src/test/java/com/project2/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
