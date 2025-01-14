aa11465c5b18767a107cbb466a3c0184dd2a6daa
min <min1225y@gmail.com>
2025-01-14 21:13:50 +0900

    대기 리다이렉트 / 토큰 로직 추가

--- flow/src/main/java/com/project2/flow/controller/UserQueueController.java
+++ flow/src/main/java/com/project2/flow/controller/UserQueueController.java
@@ -7,8 +7,13 @@ import com.project2.flow.dto.RankNumberResponse;
 import com.project2.flow.dto.RegisterUserResponse;
 import com.project2.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/queue")
@@ -32,8 +37,9 @@ public class UserQueueController {
 
     @GetMapping("/allowed")
     public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
-                                                   @RequestParam(name = "user_id") Long userId){
-        return userQueueService.isAllowed(queue,userId)
+                                                   @RequestParam(name = "user_id") Long userId,
+                                                   @RequestParam(name ="token") String token){
+        return userQueueService.isAllowedByToken(queue,userId,token)
                 .map(AllowedUserResponse::new);
     }
 
@@ -43,4 +49,22 @@ public class UserQueueController {
         return userQueueService.getRank(queue, userId)
                 .map(RankNumberResponse::new);
     }
+
+    @GetMapping("/touch")
+    Mono<?> touch(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                  @RequestParam(name = "user_id") Long userId,
+                  ServerWebExchange exchange) {
+        return Mono.defer(() -> userQueueService.generateToken(queue, userId))
+                .map(token -> {
+                    exchange.getResponse().addCookie(
+                            ResponseCookie
+                                    .from("user-queue-%s-token".formatted(queue), token)
+                                    .maxAge(Duration.ofSeconds(300))
+                                    .path("/")
+                                    .build()
+                    );
+                    return token;
+                });
+    }
+
 }
--- flow/src/main/java/com/project2/flow/controller/WaitingRoomController.java
+++ flow/src/main/java/com/project2/flow/controller/WaitingRoomController.java
@@ -6,17 +6,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.Rendering;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
 public class WaitingRoomController {
     private final UserQueueService userQueueService;
+
+    //대기등록,웹페이지 필요한 데이터를 전달
     @GetMapping("/waiting-room")
     Mono<Rendering> waitingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
                                     @RequestParam(name = "user_id") Long userId,
-                                    @RequestParam(name = "redirect_url") String redirectUrl){
-        return userQueueService.isAllowed(queue,userId)
+                                    @RequestParam(name = "redirect_url") String redirectUrl,
+                                    ServerWebExchange exchange){
+        var key = "user-queue-%s-token".formatted(queue);
+        var cookieValue = exchange.getRequest().getCookies().getFirst(key);
+        var token = (cookieValue == null) ? "" : cookieValue.getValue();
+
+        return userQueueService.isAllowedByToken(queue,userId,token)
                 .filter(allowed -> allowed)
                 .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build()))
                 .switchIfEmpty(
